4a0dfb03f71925888f31afdeac9c61255eb136ab
“[fabiglahn] <[fabian-glahn@hotmail.de]>
2021-10-15 16:35:01 +0200

    trying to implement calculating functions

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-import { createAddComponent } from "./lib/add.js";
+import { createButtonComponent } from "./lib/functions.js";
 import { createExplComponent } from "./lib/explanation.js";
-import { createInputComponent } from "./lib/inputbox.js";
 import { createTitleComponent } from "./lib/titleComponent.js";
 
 // Declare a function to create the elements and logic inside the #app element
 function createApp() {
   // Get a reference to the <div id="app"></div> element
   const appElement = document.querySelector("#app");
+
   const titleComponent = createTitleComponent();
   appElement.append(titleComponent);
+
   const explComponent = createExplComponent();
   appElement.append(explComponent);
-  const formElement = createInputComponent();
-  appElement.append(formElement);
-  const addElement = createAddComponent();
+
+  const inputElement = createInputComponent();
+  appElement.append(inputElement);
+
+  const addElement = createButtonComponent();
   appElement.append(addElement);
 }
 
